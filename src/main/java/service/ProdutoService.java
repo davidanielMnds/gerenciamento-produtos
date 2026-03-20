fc5b57fd9b5c3033d4700647b13de727abd94667
Davi Daniel <davidaniel.mivi@gmail.com>
2026-03-20 19:07:21 -0300

    feat: criar metodo de atualizar produtos em ProdutoService

--- src/main/java/service/ProdutoService.java
+++ src/main/java/service/ProdutoService.java
@@ -1,19 +1,23 @@
 package service;
+import exception.EntradaInvalidaException;
+import exception.PrecoMenor0Exception;
 import exception.ProdutoDuplicadoException;
 import exception.ProdutoNaoEncontradoException;
+import exception.QuantidadeMenor0Exception;
 import java.sql.SQLException;
 import java.util.List;
 import model.Produto;
 import repository.ProdutoDAO;
+import util.ConversorService;
 
 public class ProdutoService {
-    //--------------------------ATRIBUTO--------------------
+    //--------------------------ATRIBUTO----------------------
     private static ProdutoService instance;
     
     //--------------------------CONSTRUTOR--------------------
     public ProdutoService() {}
     
-    //--------------------------SINGLETON--------------------
+    //--------------------------SINGLETON---------------------
     public static ProdutoService getInstance()
     {
         if(instance==null)
@@ -53,9 +57,39 @@ public class ProdutoService {
         ProdutoDAO.getInstance().adicionarProduto(nome, quantidade, preco);
     }
     
+    //--------------------LISTAR PRODUTOS------------------------------------
     public List<Produto> listarProdutos() throws SQLException
     {
         return ProdutoDAO.getInstance().listarProdutos();
+    }
+    
+    public void atualizarProduto(Integer id, String nome, String quantidadeTexto, String precoTexto)
+            throws ProdutoDuplicadoException, ProdutoNaoEncontradoException,
+            PrecoMenor0Exception, QuantidadeMenor0Exception, SQLException, EntradaInvalidaException
+    {
+        if(!produtoExiste(id)) {throw new ProdutoNaoEncontradoException(id);}
+        Produto produto = getProduto(id);
+        //---------------------VERIFICAR NOME---------------------
+        if(!nome.isBlank())
+        {
+            if(produtoExiste(nome)) {throw new ProdutoDuplicadoException(nome);}
+            produto.setNome(nome);
+        }
+        //---------------------VERIFICAR QUANTIDADE---------------------
+        if(!quantidadeTexto.isBlank())
+        {
+            int quantidade = ConversorService.converterInt(quantidadeTexto, "quantidade");
+            if (quantidade<0) {throw new QuantidadeMenor0Exception(quantidade);}
+            produto.setQuantidade(quantidade);
+        }
+        //---------------------VERIFICAR QUANTIDADE---------------------
+        if(!precoTexto.isBlank())
+        {
+            double preco = ConversorService.converterDouble(precoTexto, "preço");
+            if (preco < 0) {throw new PrecoMenor0Exception(preco);}
+            produto.setPreco(preco);
+        }
+        ProdutoDAO.getInstance().atualizarProtudo(produto, id);
     }
     
     
@@ -70,7 +104,7 @@ public boolean produtoExiste(String nome) X
 --------------------------METODOS PARA ADICIONAR--------------------
 public void adicionarProduto(String nome, int quantidade, double preco) X
 public List<Produto> listarProdutos() X
-public atualizarProduto(Produto produtoAtualizado, Integer id)
+public atualizarProduto(Produto produtoAtualizado, Integer id) X
 {
     cria produto novo
     coloca as mesmas informações
